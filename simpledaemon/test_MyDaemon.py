9b50bb4ab1e4304d5797b854286bf12e03d1162a
toast254 <toast254@users.noreply.github.com>
2017-05-04 14:54:51 +0200

    Update test_MyDaemon.py

--- simpledaemon/test_MyDaemon.py
+++ simpledaemon/test_MyDaemon.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
 
-def test_do_something():
-    assert(True)
 
-def test_MyDaemon():
-    assert(True)
+def test_do_something():
+    assert True
+
+
+def test_mydaemon():
+    assert True
+
 
 def test_usage_help():
-    assert(True)
+    assert True
