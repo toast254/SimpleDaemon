a3d7ae2403013909e3b8638a4861d2169e19c992
toast254 <toast254@users.noreply.github.com>
2017-05-04 16:18:06 +0200

    Create test_MyDaemon.py

---
+++ simpledaemon/test_MyDaemon.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+import sys
+import time
+import unittest
+import MyDaemon
+
+
+class TestMyDaemon(unittest.TestCase):
+
+    def setUp(self):
+        self.daemon = MyDaemon.MyDaemon('pidfile')
+
+    def test_do_something(self):
+        self.assertIsNone(MyDaemon.do_something())
+
+    def test_usage_help(self):
+        self.assertIsNone(MyDaemon.usage_help())
+
+    @unittest.skipIf(sys.platform.startswith("win"), "not available on Windows")
+    def test_run(self):
+        self.daemon.run()
+        self.assertTrue(self.daemon.status())
+
+    def tearDown(self):
+        if self.daemon and self.daemon.status():
+            time.sleep(1)
+            self.daemon.stop()
+
+
+if __name__ == '__main__':
+    unittest.main()
