1dae187654aea119a2895762391eafa7ab1b756b
toast254 <toast254@users.noreply.github.com>
2017-05-15 11:13:22 +0200

    Update test_MyDaemon.py

--- simpledaemon/test_MyDaemon.py
+++ simpledaemon/test_MyDaemon.py
@@ -7,22 +7,34 @@ import MyDaemon
 
 
 class TestMyDaemon(unittest.TestCase):
+    """Test MyDaemon class.
+    """
 
     def setUp(self):
+        """Create a daemon object.
+        """
         self.daemon = MyDaemon.MyDaemon('pidfile')
 
     def test_do_something(self):
+        """Try to execute 'do_something' method from the daemon.
+        """
         self.assertIsNone(MyDaemon.do_something())
 
     def test_usage_help(self):
+        """Try to print usage help from the daemon.
+        """
         self.assertIsNone(MyDaemon.usage_help())
 
     @unittest.skipIf(sys.platform.startswith("win"), "not available on Windows")
     def test_run(self):
+        """Try to execute 'run' method from the daemon.
+        """
         self.daemon.run()
         self.assertTrue(self.daemon.status())
 
     def tearDown(self):
+        """Stop and remove the daemon object.
+        """
         if self.daemon and self.daemon.status():
             time.sleep(1)
             self.daemon.stop()
