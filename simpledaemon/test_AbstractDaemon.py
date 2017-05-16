6547e51b76a6e480d5f6bd40ef3db1ef25cf5659
toast254 <toast254@users.noreply.github.com>
2017-05-16 14:57:30 +0200

    Update test_AbstractDaemon.py

--- simpledaemon/test_AbstractDaemon.py
+++ simpledaemon/test_AbstractDaemon.py
@@ -3,15 +3,27 @@
 import sys
 import time
 import unittest
+import threading
 from AbstractDaemon import Daemon
 
 
-class NotAbstractDaemon(Daemon):
-    """Subclass of AbstractDaemon
+class LaunchAbstractDaemon:
+    """Launch an instance of AbstractDaemon
     """
 
-    def run(self):
-        pass  # do nothing
+    def __init__(self, pid_file: str):
+        self.pid_file = pid_file
+        self.daemon = Daemon(self.pid_file)
+
+    def start(self):
+        thread = threading.Thread(target=self.daemon.start, args=())
+        thread.daemon = True                            # Daemonize thread
+        thread.start()  # Start the execution
+
+    def restart(self):
+        thread = threading.Thread(target=self.daemon.restart, args=())
+        thread.daemon = True                            # Daemonize thread
+        thread.start()  # Start the execution
 
 
 class TestAbstractDaemon(unittest.TestCase):
@@ -21,79 +33,74 @@ class TestAbstractDaemon(unittest.TestCase):
     def setUp(self):
         """Create a new Daemon obect.
         """
-        self.daemon = NotAbstractDaemon('/tmp/test.pid')
+        self.pid_file = '/tmp/test.pid'
+        self.daemon = LaunchAbstractDaemon(self.pid_file)
 
     @unittest.skipIf(sys.platform.startswith("win"), "not available on Windows")
     def test_start(self):
         """Try to start a daemon.
         """
-        with self.assertRaises(SystemExit) as cm:
-            self.daemon.start()
-            print(cm)
-        self.assertTrue(self.daemon.status())
+        self.daemon.start()
+        time.sleep(1)
+        self.assertTrue(Daemon(self.pid_file).status())
 
     @unittest.skipIf(sys.platform.startswith("win"), "not available on Windows")
     def test_stop(self):
         """Try to stop a daemon.
         """
-        with self.assertRaises(SystemExit) as cm:
-            self.daemon.start()
-            print(cm)
+        self.daemon.start()
         time.sleep(1)
-        self.daemon.stop()
-        self.assertFalse(self.daemon.status())
+        Daemon(self.pid_file).stop()
+        time.sleep(1)
+        self.assertFalse(Daemon(self.pid_file).status())
 
     @unittest.skipIf(sys.platform.startswith("win"), "not available on Windows")
     def test_restart(self):
         """Try to restart a daemon.
         """
-        with self.assertRaises(SystemExit) as cm:
-            self.daemon.restart()
-            print(cm)
-        self.assertTrue(self.daemon.status())
+        self.daemon.restart()
+        time.sleep(1)
+        self.assertTrue(Daemon(self.pid_file).status())
 
     def test_run(self):
         with self.assertRaises(NotImplementedError):
-            self.daemon.run()
+            Daemon(self.pid_file).run()
 
     def test_status(self):
         """Try to check status of a daemon.
         """
-        self.assertFalse(self.daemon.status())
+        self.assertFalse(Daemon(self.pid_file).status())
 
     @unittest.skipIf(sys.platform.startswith("win"), "not available on Windows")
     def test_check_pid_stopped(self):
         """Try to check pid of a stopped daemon.
         """
-        self.assertFalse(self.daemon.check_pid())
-        with self.assertRaises(SystemExit) as cm:
-            self.daemon.start()
-            print(cm)
+        self.assertFalse(Daemon(self.pid_file).check_pid())
+        self.daemon.start()
         time.sleep(1)
-        self.daemon.stop()
-        self.assertFalse(self.daemon.check_pid())
+        Daemon(self.pid_file).stop()
+        time.sleep(1)
+        self.assertFalse(Daemon(self.pid_file).check_pid())
 
     @unittest.skipIf(sys.platform.startswith("win"), "not available on Windows")
     def test_check_pid_started(self):
         """Try to check pid of a started daemon.
         """
-        with self.assertRaises(SystemExit) as cm:
-            self.daemon.start()
-            print(cm)
-        self.assertTrue(self.daemon.check_pid())
+        self.daemon.start()
+        time.sleep(1)
+        self.assertTrue(Daemon(self.pid_file).check_pid())
 
     def test_del_pid(self):
         """Try to remove pid file of a daemon.
         """
         with self.assertRaises(FileNotFoundError):
-            self.daemon.delpid()
+            Daemon(self.pid_file).delpid()
 
     def tearDown(self):
         """Stop and delete the daemon object.
         """
-        if self.daemon and self.daemon.status():
-            time.sleep(1)
-            self.daemon.stop()
+        if self.daemon and Daemon(self.pid_file).status():
+            Daemon(self.pid_file).stop()
 
 
 if __name__ == '__main__':
